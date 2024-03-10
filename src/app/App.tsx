ebf9aae16ef71b6ec7f73b6be5a927b97e4997a6
N0FreeLunch <consilience90@gmail.com>
2024-03-11 01:57:10 +0900

    date pickerにlocale追加

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Routing from '@src/pages/Routing';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+// If you are using date-fns v3.x, please import the v3 adapter
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { ja } from 'date-fns/locale';
 
 function App() {
   return (
     <>
-      <Routing />
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
+        <Routing />
+      </LocalizationProvider>
     </>
   )
 }
